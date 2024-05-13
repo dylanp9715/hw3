174cd3e920790c56672fb26bc269c64aa10c8aca
Dylan Park <dylan.park@kellogg.northwestern.edu>
2024-05-13 21:50:16 +0000

    Places and Entries functionality seems to be fully working

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,4 +1,31 @@
 class EntriesController < ApplicationController
-  def index
+  def show
+    # render entries/show view with details about Entry
+    @entry = Entry.find_by({"id" => params["id"]})
+    @place = Place.find_by({"id" => @entry["place_id"]})
+  end
+
+  def new
+    # render entries/new view with new Entry form
+    @place = Place.find_by({"id" => params["place_id"]})
+  end
+
+  def create
+    # start with a new Entry
+    @entry = Entry.new
+
+    # assign user-entered form data to Entry's columns
+    @entry["title"] = params["title"]
+    @entry["description"] = params["description"]
+    @entry["posted_on"] = params["posted_on"]
+
+    # assign relationship between Entry and Place
+    @entry["place_id"] = params["place_id"]
+
+    # save Entry row
+    @entry.save
+
+    # redirect user
+    redirect_to "/places/#{@entry["place_id"]}"
   end
 end
